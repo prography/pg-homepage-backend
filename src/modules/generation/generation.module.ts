6b4ccfebe96f06109be7869f4cafd1a6f9d76447
linho <linho301150@gmail.com>
2022-10-19 18:57:50 +0900

    Init: generation module 추가

---
+++ src/modules/generation/generation.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Generations } from 'src/infra/entity/Generations.entity';
+import { GenerationController } from './controller/generation.controller';
+import { GenerationRepository } from './repository/generation.repository';
+import { GenerationService } from './service/generation.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Generations])],
+  controllers: [GenerationController],
+  providers: [GenerationService, GenerationRepository],
+})
+export class GenerationModule {}
