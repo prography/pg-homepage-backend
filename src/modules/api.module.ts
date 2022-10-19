c6308570e24edec18ec062503a1ca64152507abd
linho <linho301150@gmail.com>
2022-10-19 18:55:40 +0900

    Init: generation 모듈 추가 #8

--- src/modules/api.module.ts
+++ src/modules/api.module.ts
@@ -1,7 +1,8 @@
 import { HealthCheckModule } from '@core/health-check';
 import { Module } from '@nestjs/common';
+import { GenerationModule } from './generation/generation.module';
 
 @Module({
-  imports: [HealthCheckModule],
+  imports: [HealthCheckModule, GenerationModule],
 })
 export class ApiModule {}
