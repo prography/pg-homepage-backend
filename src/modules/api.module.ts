d6d10ba085f01c886cfd9213ea4f03235d782ebd
linho <linho301150@gmail.com>
2022-11-03 19:11:11 +0900

    Fix: AWS 모듈과 PortFolio 모듈 등록 #18

--- src/modules/api.module.ts
+++ src/modules/api.module.ts
@@ -1,9 +1,17 @@
 import { HealthCheckModule } from '@core/health-check';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { AwsModule } from './aws/aws.module';
 import { GenerationModule } from './generation/generation.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
 
 @Module({
-  imports: [HealthCheckModule, GenerationModule, AuthModule],
+  imports: [
+    HealthCheckModule,
+    GenerationModule,
+    AuthModule,
+    PortfolioModule,
+    AwsModule,
+  ],
 })
 export class ApiModule {}
