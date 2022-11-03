02191cd1b0901980bba334c03c94ce8632d75b32
linho <linho301150@gmail.com>
2022-11-03 19:08:32 +0900

    Feat: Portfolio 관련 DTO 추가

---
+++ src/modules/portfolio/dto/create-portfolio.dto.ts
@@ -0,0 +1,51 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsJSON, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class PortfolioControllerDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    example: '6팀',
+    description: '팀명',
+    required: true,
+  })
+  readonly teamName: string;
+
+  @IsNotEmpty()
+  @IsJSON()
+  @ApiProperty({
+    example: `["신성환","정유진","강동하","송은우","이한나","이재준"]`,
+    description: '멤버',
+    required: true,
+  })
+  readonly teamMembers: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    example: 'pilit',
+    description: '프로젝트명',
+    required: true,
+  })
+  readonly projectName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    example: '알약 복용여부를 확인해주는 서비스 입니다.',
+    description: '프로젝트 설명',
+    required: true,
+  })
+  readonly projectDescription: string;
+}
+
+export class PortfolioCreateDto extends PortfolioControllerDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty({
+    example: '3',
+    description: '기수 고유번호',
+    required: true,
+  })
+  readonly generationId: number;
+}
