1611bd47bdc7a0bed62abc377fcbbe10271196d4
Be-student <presentsong@naver.com>
2022-11-18 17:19:26 +0900

    feat: user api 타입 정의 추가

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -1,46 +1,106 @@
+import { JwtAuthGuard } from '@modules/auth/jwt/guard/jwt.guard';
+import { ErrorDto } from '@modules/common/dto/error.dto';
 import {
   Body,
   Controller,
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Post,
   Put,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { Request } from 'express';
 import { Users } from 'src/infra/entity/Users.entity';
 import { UserCreateDto } from '../dto/create-user.dto';
-import { UserPutDto } from '../dto/put-user.dto';
-import { UserService } from '../service/user.service';
+import {
+  UserAuthDto,
+  UserChangedResultDto,
+  UserPutDto,
+} from '../dto/put-user.dto';
+import { UserService, UserType } from '../service/user.service';
 
+type RequestWithToken = {
+  user: UserType;
+};
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @ApiOperation({ summary: '사용자를 생성합니다' })
+  @ApiForbiddenResponse({
+    description: '이미 있는 사용자 입니다',
+    type: ErrorDto,
+  })
   @Post('/')
   async createUser(@Body() userCreateDto: UserCreateDto): Promise<Users> {
     return this.userService.userCreate(userCreateDto);
   }
 
   @ApiOperation({ summary: '사용자로 로그인합니다' })
+  @ApiForbiddenResponse({
+    description: '존재하지 않는 사용자 입니다',
+    type: ErrorDto,
+  })
   @Post('/login')
-  async login(@Body() userCreateDto: UserCreateDto) {
+  async login(@Body() userCreateDto: UserCreateDto): Promise<UserAuthDto> {
     return this.userService.login(userCreateDto);
   }
 
-  @Get('/:userId')
-  async getUser(@Param('userId') userId: string): Promise<Users> {
-    return null;
-  }
-
+  @ApiOperation({
+    summary: '사용자의 개인정보를 변경합니다',
+    description: '등록한 사용자이거나, 어드민만 변경 가능합니다',
+  })
+  @ApiUnauthorizedResponse({
+    description: '권한이 없는 사용자 입니다',
+    type: ErrorDto,
+  })
+  @ApiNotFoundResponse({
+    description: '존재하지 않거나 본인이 아닙니다',
+    type: ErrorDto,
+  })
+  @ApiBearerAuth('access-token')
+  @UseGuards(AuthGuard('jwt'))
   @Put('/:userId')
   async putUser(
-    @Body('userId') userId: string,
+    @Req() req: RequestWithToken,
+    @Param('userId', new ParseIntPipe()) userId: number,
     @Body() userPutDto: UserPutDto,
-  ) {}
+  ): Promise<UserChangedResultDto> {
+    return await this.userService.update(userPutDto, req.user, userId);
+  }
 
+  @ApiOperation({
+    summary: '사용자를 제거합니다',
+    description: '어드민 전용 api',
+  })
+  @ApiUnauthorizedResponse({
+    description: '권한이 없는 사용자 입니다',
+    type: ErrorDto,
+  })
+  @ApiNotFoundResponse({
+    description: '존재하지 않는 사용자입니다',
+    type: ErrorDto,
+  })
+  @ApiBearerAuth('access-token')
+  @UseGuards(AuthGuard('jwt'))
   @Delete('/:userId')
-  async deleteUser(@Param('userId') userId: number) {}
+  async deleteUser(
+    @Req() req: Request,
+    @Param('userId') userId: number,
+  ): Promise<UserChangedResultDto> {
+    return await this.userService.delete(req.user, userId);
+  }
 }
--- src/modules/user/dto/put-user.dto.ts
+++ src/modules/user/dto/put-user.dto.ts
@@ -1,4 +1,16 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { Users } from 'src/infra/entity/Users.entity';
 import { UserCreateDto } from './create-user.dto';
 
 export class UserPutDto extends PartialType(UserCreateDto) {}
+export class UserChangedResultDto {
+  @ApiProperty({ description: '변경된 row 갯수', example: '1' })
+  affected: number;
+}
+
+export class UserAuthDto {
+  @ApiProperty({ description: '사용자 정보' })
+  user: Users;
+  @ApiProperty({ description: '사용자 id로 관리되는 토큰' })
+  token: string;
+}
