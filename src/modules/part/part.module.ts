841da7392a093c4730a274883842d0fc1e9e5e20
be-student <presentsong@naver.com>
2022-12-18 14:56:31 +0900

    fix: 빠진 기능 수정

--- src/modules/part/part.module.ts
+++ src/modules/part/part.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PartService } from './service/part.service';
 import { PartController } from './controller/part.controller';
 import { PartRepository } from './repository/part.repository';
+import { GenerationModule } from '@modules/generation/generation.module';
 
 @Module({
+  imports: [GenerationModule],
   controllers: [PartController],
   providers: [PartService, PartRepository],
 })
