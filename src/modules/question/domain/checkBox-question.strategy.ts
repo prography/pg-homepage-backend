f4d184d7f7e921204bffa4c2bb5ce3d5aa1e9fba
Be-student <presentsong@naver.com>
2022-12-29 22:58:01 +0900

    fix: 체크박스의 경우 선택지가 없도록 수정한다

--- src/modules/question/domain/checkBox-question.strategy.ts
+++ src/modules/question/domain/checkBox-question.strategy.ts
@@ -5,7 +5,6 @@ import {
 } from '../dto/create-question.dto';
 import { PartQuestionRepository } from '../repositories/parts-questions.repository';
 import { QuestionRepository } from '../repositories/question.repository';
-import { SelectOptionsRepository } from '../repositories/select-options.repository';
 import { QuestionStrategy } from './question.strategy';
 
 export class CheckBoxQuestion implements QuestionStrategy {
@@ -26,25 +25,19 @@ export class CheckBoxQuestion implements QuestionStrategy {
   }
 
   private validate(): void {
-    if (this.selectOptions?.length == 0) {
-      throw new BadRequestException('빈 데이터');
+    if (
+      this.selectOptions?.length !== undefined &&
+      this.selectOptions?.length !== 0
+    ) {
+      throw new BadRequestException('선택지가 있으면 안됩니다');
     }
   }
 
   async saveQuestion(
     questionRepository: QuestionRepository,
     partQuestionRepository: PartQuestionRepository,
-    selectOptionsRepository: SelectOptionsRepository,
   ): Promise<number> {
     const { id } = await questionRepository.save(this.data);
-    await Promise.all(
-      this.selectOptions.map(async (selectOption) => {
-        await selectOptionsRepository.save({
-          ...selectOption,
-          questionId: id,
-        });
-      }),
-    );
     await Promise.all(
       this.partIds.map(async (partId) => {
         await partQuestionRepository.save({
